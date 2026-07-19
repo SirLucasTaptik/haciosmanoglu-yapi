f87e645a2c35a2538d3912509ad3dc9c5549e264
SirLucas <101318158+SirLucasTaptik@users.noreply.github.com>
2026-07-20 00:22:49 +0400

    Update sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,5 +1,6 @@
+console.log("SANITY PROJECT:", process.env.NEXT_PUBLIC_SANITY_PROJECT_ID);
+console.log("SANITY DATASET:", process.env.NEXT_PUBLIC_SANITY_DATASET);
 import { defineCliConfig } from "sanity/cli";
-
 export default defineCliConfig({
   api: {
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
